40469cc264439d1abb6bd0d0410fd1d912b0108f
Dodanim Josue Diaz Martinez <josuediaz282@hotmail.com>
2024-05-02 13:00:08 -0600

    Form1 cambio

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,21 +53,22 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.btnIngresar);
             this.groupBox1.Controls.Add(this.txtContra);
             this.groupBox1.Controls.Add(this.txtUsuario);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(317, 99);
+            this.groupBox1.Location = new System.Drawing.Point(12, 76);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(451, 300);
+            this.groupBox1.Size = new System.Drawing.Size(776, 348);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
             // btnIngresar
             // 
-            this.btnIngresar.Location = new System.Drawing.Point(76, 208);
+            this.btnIngresar.Location = new System.Drawing.Point(326, 244);
             this.btnIngresar.Name = "btnIngresar";
             this.btnIngresar.Size = new System.Drawing.Size(276, 58);
             this.btnIngresar.TabIndex = 4;
@@ -77,14 +78,14 @@
             // 
             // txtContra
             // 
-            this.txtContra.Location = new System.Drawing.Point(235, 145);
+            this.txtContra.Location = new System.Drawing.Point(496, 164);
             this.txtContra.Name = "txtContra";
             this.txtContra.Size = new System.Drawing.Size(171, 30);
             this.txtContra.TabIndex = 3;
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(235, 62);
+            this.txtUsuario.Location = new System.Drawing.Point(496, 73);
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(171, 30);
             this.txtUsuario.TabIndex = 2;
@@ -94,7 +95,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(34, 145);
+            this.label3.Location = new System.Drawing.Point(287, 161);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(161, 32);
             this.label3.TabIndex = 1;
@@ -105,7 +106,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(55, 62);
+            this.label2.Location = new System.Drawing.Point(320, 73);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(112, 32);
             this.label2.TabIndex = 0;
@@ -115,7 +116,7 @@
             // 
             this.pictureBox1.ErrorImage = global::Menu_Restaurante.Properties.Resources.Login;
             this.pictureBox1.Image = global::Menu_Restaurante.Properties.Resources.Login;
-            this.pictureBox1.Location = new System.Drawing.Point(56, 138);
+            this.pictureBox1.Location = new System.Drawing.Point(34, 42);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(183, 192);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
@@ -127,7 +128,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
